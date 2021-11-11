6b8436c90c7b6bf4aad7ba4c31b62969d4eb33d9
Ashley <adunderwood96@gmail.com>
2021-11-11 16:46:03 -0500

    arrays var added/edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-// Start of PW Generator
 function generatePassword() {
 
-// Array of possible characters
-var numbers = ["0123456789"];
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var special = ["!@#$%^&*()"];
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    // Array of possible characters
+    var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var specialArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
+    function questions()
 
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
 
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+        passwordText.value = password;
 
-    passwordText.value = password;
+    }
 
-}
-
-generateBtn.addEventListener("click", writePassword);
+    generateBtn.addEventListener("click", writePassword);
