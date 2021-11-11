3501df798db05753890d09480d1faf1b146780d8
Ashley <adunderwood96@gmail.com>
2021-11-11 15:32:09 -0500

    added var for required characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 // Assignment code here
-
-
 var generateBtn = document.querySelector("#generate");
 
+// Start of PW Generator
 function generatePassword() {
+
+// Array of possible characters
+var numbers = ["0123456789"];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var special = ["!@#$%^&*()"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+
 
 function writePassword() {
     var password = generatePassword();
