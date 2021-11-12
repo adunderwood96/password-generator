0e9ffb20c5ddb476a7f0639fea2b925998bf2d38
Ashley <adunderwood96@gmail.com>
2021-11-11 22:32:38 -0500

    error fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,74 @@
 // Assignment code here
-
 var generateBtn = document.querySelector("#generate");
-
-// Character Arrays
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var special = ['@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '' = '', '+'];
 
 function generatePassword() {
 
-    //Conditions of Password requirements 
+    // Character Arrays
+    var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var lowerAlphaArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var upperAlphaArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var specialArray = ['@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '' = '', '+'];
+
+    var newPwd = [];
+    var pwdCharacters = [];
+
+    // Generate Password Prompts
+    var pwdLength = prompt("How many characters would you like your password to contain?");
+    var numbers = confirm("Do you want numbers in your password?");
+    var upperAlpha = confirm("Do you want numbers in your password?");
+    var lowerAlpha = confirm("you want lowercase letters in your password?")
 
     // Password Length
-    var pwdLength = "" {
-        while (pwdLength < 8 || pwdLength > 128)
-    alert("Password must be between 8 to 128 characters in length");
-    var pwdLength = prompt("How many characters would you like your password to contain?");
 
-}
+    while (pwdLength < 8 || pwdLength > 128) {
+        alert("Password must be between 8 to 128 characters in length");
+    }
+    // Let User know how many characters will be in password
+    alert('Your password will contain ${pwdLength} characters');
 
-// Let User know how many characters will be in password
-alert('Your password will contain ${pwdLength} characters');
+    // Password Character Contains Conditions:
+    if (numbers) {
+        pwdCharacters = pwdCharacters.concat(numbersArray);
 
-// Password Character Contains Conditions:
-var pwdCharacters = [] {
+    }
 
-    if (confirm("Do you want numbers in your password?")) {
-    pwdCharacters = pwdCharacters.concat(numbers);
+    if (upperAlpha) {
+        pwdCharacters = pwdCharacters.concat(upperAlphaArray);
 
-}
+    }
 
-if (confirm("Do you want Uppercase letters in your password?")) {
-    pwdCharacters = pwdCharacters.concat(upper);
+    if (lowerAlpha) {
+        pwdCharacters = pwdCharacters.concat(lowerAlphaArray);
 
-}
+    }
 
-if (confirm("you want lowercase letters in your password?")) {
-    pwdCharacters = pwdCharacters.concat(lower);
+    if (special) {
+        pwdCharacters = pwdCharacters.concat(specialArray);
 
-}
+    }
 
-if (confirm("you want special characters in your password?"));
-pwdCharacters = pwdCharacters.concat(special);
+    console.log(newPwd)
+    // result
+    var newPwd = ""
 
-}
+    for (newPwd i = 0; i < pwdLength = i++); {
+        newPwd = newPwd + pwdCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+    }
 
-console.log(newPwd)
-// result
-var newPwd = "" {
+    return newPwd.join("");
 
-    for (var i = 0; i < pwdLength; i++) {
-    newPwd = newPwd + pwdCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-    console.log(randomPassword)
-}
+    // Write password
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
 
-return newPwd;
+        passwordText.value = password;
 
-// Write password
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    }
 
-    passwordText.value = password;
+    function copyToClipboard() {
 
-}
+    }
 
-generateBtn.addEventListener("click", writePassword); {
-}
+generateBtn.addEventListener("click", writePassword);
