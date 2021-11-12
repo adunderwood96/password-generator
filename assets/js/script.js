02f4fcef0fc7927556cf619a94233a83acf7fd7b
Ashley <adunderwood96@gmail.com>
2021-11-11 20:05:58 -0500

    working on errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,52 +10,52 @@ var special = ['@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '' = '', '
 
 function generatePassword() {
 
-//Conditions of Password requirements 
+    //Conditions of Password requirements 
 
     // Password Length
     var pwdLength = "" {
-    while (pwdLength < 8 || pwdLength >128)
-        alert("Password must be between 8 to 128 characters in length");
-        var pwdLength = prompt("How many characters would you like your password to contain?");
+        while (pwdLength < 8 || pwdLength > 128)
+    alert("Password must be between 8 to 128 characters in length");
+    var pwdLength = prompt("How many characters would you like your password to contain?");
 
-    }
-    
-    // Let User know how many characters will be in password
-    alert('Your password will contain ${pwdLength} characters');
+}
 
-    // Password Character Contains Conditions:
-    var pwdCharacters = [] {
+// Let User know how many characters will be in password
+alert('Your password will contain ${pwdLength} characters');
+
+// Password Character Contains Conditions:
+var pwdCharacters = [] {
 
     if (confirm("Do you want numbers in your password?")) {
-        pwdCharacters = pwdCharacters.concat(numbers);
+    pwdCharacters = pwdCharacters.concat(numbers);
 
-    }
+}
 
-    if (confirm("Do you want Uppercase letters in your password?")) {
-        pwdCharacters = pwdCharacters.concat(upper);
+if (confirm("Do you want Uppercase letters in your password?")) {
+    pwdCharacters = pwdCharacters.concat(upper);
 
-    }
+}
 
-    if (confirm("you want lowercase letters in your password?")) {
-        pwdCharacters = pwdCharacters.concat(lower);
+if (confirm("you want lowercase letters in your password?")) {
+    pwdCharacters = pwdCharacters.concat(lower);
 
-    }
+}
 
-    if (confirm("you want special characters in your password?"));
-    pwdCharacters = pwdCharacters.concat(special);
+if (confirm("you want special characters in your password?"));
+pwdCharacters = pwdCharacters.concat(special);
 
 }
 
 console.log(newPwd)
 // result
-var newPwd= "" {
-      
-      for (var i = 0; i < pwdLength; i++) {
-        newPwd = newPwd + pwdCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-        console.log(randomPassword)
-      }
+var newPwd = "" {
 
-      return newPwd;
+    for (var i = 0; i < pwdLength; i++) {
+    newPwd = newPwd + pwdCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
+}
+
+return newPwd;
 
 // Write password
 function writePassword() {
