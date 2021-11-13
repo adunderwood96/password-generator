a7c6223244140f9b48c999e69e46c9416363c9ab
Ashley <adunderwood96@gmail.com>
2021-11-12 22:35:19 -0500

    working to see why js will not work/changing var names layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 // Assignment code here
-document.querySelector("#generate").addEventListener("click", writePassword);
 
 var generateBtn = document.querySelector("#generate");
-var copyToClipBoardButton = document.getElementById("copytoclipboard");
-
+console.log(generateBtn)
 
 // Character Arrays
 var numbersArray = /[0-9]/g;
@@ -11,70 +9,71 @@ var lowerAlphaArray = /[a-z]/g;
 var upperAlphaArray = /[A-Z]/g;
 var specialArray = ['@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '' = '', '+'];
 
-var pwdCharacters = [];
-
+var pwdCharacters = "";
 
 // Prompts for Password
 function generatePassword() {
-
+  console.log("btn was clicked")
   // Loop if length does not meet requirements
-  while(pwdLength <= 8 || pwdLength >= 128); {
-    var pwdLength = prompt("How many characters would you like your password to contain?");
-  }
+  var pwdLength = prompt("How many characters would you like your password to contain?");
+  while (pwdLength < 8 || pwdLength > 128 || isNaN(parseInt(pwdLength))) {
+    alert("Password must be between 8 to 128 characters iin length!")
+  };
 
   // Let User know how many characters will be in password
   alert(`Your password will contain ${pwdLength} characters`);
 
   // Password Character Contains Conditions:
-  if(numbers) {
+  if (numbers) {
     var numbers = confirm("Click OK if you would like numbers in your password");
-    pwdCharacters = pwdCharacters.concat(numbersArray);
+    pwdCharacters += numbersArray;
 
-  }
+  };
 
-  if(upperAlpha) {
+  if (upperAlpha) {
     var upperAlpha = confirm("Click OK if you would like uppercase letters in your password");
-    pwdCharacters = pwdCharacters.concat(upperAlphaArray);
+    pwdCharacters += upperAlphaArray;
 
-  }
+  };
 
-  if(lowerAlpha) {
+  if (lowerAlpha) {
     var lowerAlpha = confirm("Click OK if you would like lowercase letters in your password");
-    pwdCharacters = pwdCharacters.concat(lowerAlphaArray);
+    pwdCharacters += lowerAlphaArray
 
-  }
+  };
 
-  if(special) {
+  if (special) {
     var special = confirm("Click OK if you would like special characters, such as '!@#$^&*-_=+' in your password");
-    pwdCharacters = pwdCharacters.concat(specialArray);
-  }
-  console.log(pwdCharacters)
+    pwdCharacters += specialArray
 
-var = newPwd = [];
-for (var i = 0; i < confirmLength; i++) {
-    newPwd = newPwd + pwdCharacters.push[Math.floor(Math.random() * pwdCharacters.length)];
+  };
+  // console.log("pwdCharacters worked")
+
+  if (
+    numbers === false &&
+    upperAlpha === false &&
+    lowerAlpha === false &&
+    special === false
+  ) {
+    generatePassword();
+  }
+}
+
+  var newPwd = "";
+  for (var i = 0; i < confirmLength; i++) {
+    newPwd += pwdCharacters.charAt[Math.floor(Math.random() * pwdCharacters.length)];
     console.log(newPwd)
   }
 
-  return newPwd.join("");
-}
+  return newPwd("");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
-
-function copyToClipboard() {
-  document.getElementById("display").select();
-
-  document.execCommand("Copy");
-
-  alert("Your password has now been copied to the clipboard");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+console.log(generateBtn)
